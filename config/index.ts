e4cd6c336066e49f8e0b5524215a33997f37a14f
Shivak11 <121174127+Shivak11@users.noreply.github.com>
2023-09-19 07:56:26 +0530

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,11 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const APP_ID = `e7d56e68-9cc1-44dd-8a26-c3275c1b8d5f`
+export const API_KEY = `app-yLElvcPkzfarA6OjZRcHILZX`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
+  title: 'Title Engine',
+  description: 'Paste in your boring academic study title. Get a hook-y one in return!',
+  copyright: 'Precise Words',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
@@ -14,4 +14,4 @@ export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
-export const DEFAULT_VALUE_MAX_LEN = 48
+export const DEFAULT_VALUE_MAX_LEN = 150
